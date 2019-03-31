PixelExperience/old_frameworks_base

SystemUI: Refresh themes on dirty flash
Change-Id: I54263e6196387ce9f5eb914e76b8d999bed6b7a2

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -93,6 +93,7 @@ import android.media.MediaMetadata;
 import android.metrics.LogMaker;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -4076,6 +4077,14 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected void updateTheme() {
         updateTheme(false);
     }
+    
+    private boolean themeNeedsRefresh(){
+        if (mContext.getSharedPreferences("systemui_theming", 0).getString("build_fingerprint", "").equals(Build.CUSTOM_FINGERPRINT)){
+            return false;
+        }
+        mContext.getSharedPreferences("systemui_theming", 0).edit().putBoolean("build_fingerprint", Build.CUSTOM_FINGERPRINT).commit();
+        return true;
+    }
 
     protected void updateTheme(boolean fromPowerSaveCallback) {
         final boolean inflated = mStackScroller != null && mStatusBarWindowManager != null;
@@ -4087,7 +4096,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             darkThemeNeeded = true;
         }
         final boolean useDarkTheme = darkThemeNeeded;
-        if (isUsingDarkTheme() != useDarkTheme) {
+        if (themeNeedsRefresh() || isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
                 umm.setNightMode(useDarkTheme ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
                 try {

Destiny911gtr/android_frameworks_base-1

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -91,6 +91,7 @@ import android.media.session.PlaybackState;
 import android.metrics.LogMaker;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Process;
@@ -4188,6 +4189,14 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     /**
      * Switches theme from light to dark and vice-versa.
      */
+   private boolean themeNeedsRefresh(){
+        if (mContext.getSharedPreferences("systemui_theming", 0).getString("build_fingerprint", "").equals(Build.ARROW_FINGERPRINT)){
+            return false;
+	}
+	mContext.getSharedPreferences("systemui_theming", 0).edit().putString("build_fingerprint", Build.ARROW_FINGERPRINT).commit();
+        return true;
+    }
+
     protected void updateTheme() {
         final boolean inflated = mStackScroller != null && mStatusBarWindowManager != null;
         int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
@@ -4223,7 +4232,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             unfuckBlackWhiteAccent();
         }
 
-        if (isUsingDarkTheme() != useDarkTheme) {
+        if (themeNeedsRefresh() || isUsingDarkTheme() != useDarkTheme) {
                 try {
                     mOverlayManager.setEnabled("com.android.system.theme.dark.arrow",
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
@@ -4243,7 +4252,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             }
         }
 
-        if (isUsingBlackTheme() != useBlackTheme) {
+        if (themeNeedsRefresh() || isUsingBlackTheme() != useBlackTheme) {
                 try {
                     mOverlayManager.setEnabled("com.android.system.theme.black.arrow",
                             useBlackTheme, mLockscreenUserManager.getCurrentUserId());